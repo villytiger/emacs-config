06b9f83aa497fbeac05a217556465b83561be4e0
Ilya Lyubimov <villytiger@gmail.com>
2023-12-10 15:48:12 +0100

    temp

--- early-init.el
+++ early-init.el
@@ -1,4 +1,10 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
 
+;;(setq gc-cons-threshold most-positive-fixnum)
+
+;; Disable built-in package.el. Borg is used instead.
 (setq package-enable-at-startup nil)
+
+;; Let it be maximized. Surprisingly, this line in early-init.el also reduces init time.
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
--- init.el
+++ init.el
@@ -1,15 +1,28 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
-(setq display-line-numbers-type t)
+;(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+;(setq display-line-numbers-type t)
 
-(use-package borg
-  :demand t
-  :load-path (lambda () (expand-file-name "lib/borg" user-emacs-directory))
-  :config
-  (when (native-comp-available-p)
-    (setq borg-compile-function #'borg-byte+native-compile))
-  (borg-initialize))
+;(use-package borg
+;  :demand t
+;  :load-path (lambda () (expand-file-name "lib/borg" user-emacs-directory))
+;  :config
+;  (when (native-comp-available-p)
+;    (setq borg-compile-function #'borg-byte+native-compile))
+  ;; Override borg-activate to disable autoloading.
+  ;; Don't clutter emacs with unused packages.
+  ;; Let use-package do its job.
+;;   (defun borg-activate (clone)
+;;     "Activate the clone named CLONE.
+
+;; Add the appropriate directories to `load-path' and
+;; `Info-directory-list'."
+;;     (interactive (list (borg-read-clone "Activate clone: ")))
+;;     ;; (dolist (dir (borg-load-path clone))
+;;       ;; (add-to-list 'load-path dir))
+;;     (dolist (dir (borg-info-path clone))
+;;       (push  dir Info-directory-list)))
+;);  (borg-initialize))
 
 ;; (setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
 ;; (defun load-from-init-dir (local-path)
@@ -17,8 +30,8 @@
 
 ;; (load-from-init-dir "elpaca")
 
-(use-package gruvbox-theme :config (load-theme 'gruvbox t))
-(use-package meow :config (load (expand-file-name "meow" user-emacs-directory)))
+;; (use-package gruvbox-theme :config (load-theme 'gruvbox t))
+;; (use-package meow :config (load (expand-file-name "meow" user-emacs-directory)))
 
 ;; (setq org-directory "~/cloud/mobile/org")
 ;; (setq org-roam-directory org-directory)
